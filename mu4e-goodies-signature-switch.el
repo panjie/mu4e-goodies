dda2d9dec587eeccbe5243f497b21f980aebd939
panjie <panjie@gmail.com>
2014-10-09 18:34:44 +0800

    change variable name: mu4e-compose-signatures -> mu4e-goodies-signatures

--- mu4e-goodies-signature-switch.el
+++ mu4e-goodies-signature-switch.el
@@ -3,25 +3,25 @@
 ;; Signature swith
 ;;============================================================
 
-(defvar mu4e-compose-signatures nil
+(defvar mu4e-goodies-signatures nil
   "List of signatures used by mu4e-goodies-signature-switch, like:
 (\"XXX/xxx@gmail.com\"  \"YYY/yyy@yahoo.com\" \"ZZZ/zzz@outlook.com\")")
 
 (defun mu4e-switch-signature ()
-  "Switch between signatures defined in mu4e-compose-signatures"
+  "Switch between signatures defined in mu4e-goodies-signatures"
   (interactive)
   (save-excursion
-    (when (> (length mu4e-compose-signatures) 0)
+    (when (> (length mu4e-goodies-signatures) 0)
       (message-goto-signature)
       (let ((sig (buffer-substring (point) (point-max)))
             (next-sig nil)
             (idx 0))
-        (dolist (elt mu4e-compose-signatures next-sig)
+        (dolist (elt mu4e-goodies-signatures next-sig)
           (setq idx (+ 1 idx))
           (when (string-equal elt sig)
-            (setq next-sig (nth idx mu4e-compose-signatures))))
+            (setq next-sig (nth idx mu4e-goodies-signatures))))
         (unless next-sig
-          (setq next-sig (car mu4e-compose-signatures)))
+          (setq next-sig (car mu4e-goodies-signatures)))
         (delete-region (point) (point-max))
         (insert next-sig)))))
 
