c3a9d7475b9df4f46102f450a0c8faa5d92fa277
panjie <panjie@gmail.com>
2014-12-15 18:42:38 +0800

    ADD: signature switch

--- mu4e-goodies-signature-switch.el
+++ mu4e-goodies-signature-switch.el
@@ -4,11 +4,18 @@
 ;;============================================================
 
 (defvar mu4e-goodies-signatures nil
-  "List of signatures used by mu4e-goodies-signature-switch, like:
-(\"XXX/xxx@gmail.com\"  \"YYY/yyy@yahoo.com\" \"ZZZ/zzz@outlook.com\")")
+  "List of signatures used by mu4e-goodies-signature-switch,
+  which is an alist like:
+((\"sig name\" . \"XXX/xxx@gmail.com\")  (\"sig name\" . \"YYY/yyy@yahoo.com\"))")
 
-(defun mu4e-switch-signature ()
-  "Switch between signatures defined in mu4e-goodies-signatures"
+(defvar mu4e-goodies-signature-switch-rules nil
+  "Rules of signature swith, which is an alist like:
+((\"rules-of-regexp\" . \"sig name\") ...)
+The rules will only apply to the first recipient's address")
+
+(defun mu4e-goodies-switch-signature (&optional signame)
+  "Switch between signatures defined in mu4e-goodies-signatures
+if signame is not given"
   (interactive)
   (save-excursion
     (when (> (length mu4e-goodies-signatures) 0)
@@ -16,16 +23,31 @@
       (let ((sig (buffer-substring (point) (point-max)))
             (next-sig nil)
             (idx 0))
-        (dolist (elt mu4e-goodies-signatures next-sig)
-          (setq idx (+ 1 idx))
-          (when (string-equal elt sig)
-            (setq next-sig (nth idx mu4e-goodies-signatures))))
-        (unless next-sig
-          (setq next-sig (car mu4e-goodies-signatures)))
-        (delete-region (point) (point-max))
-        (insert next-sig)))))
+        (unless (and signame (setq next-sig (cdr (assoc signame mu4e-goodies-signatures))))
+          (dolist (elt mu4e-goodies-signatures next-sig)
+            (setq idx (+ 1 idx))
+            (when (string-equal (cdr elt) sig)
+              (setq next-sig (cdr (nth idx mu4e-goodies-signatures))))))
+          (unless next-sig
+            (setq next-sig (cdr (car mu4e-goodies-signatures))))
+          (delete-region (point) (point-max))
+          (insert next-sig)))))
 
-(define-key mu4e-compose-mode-map "\C-cs" 'mu4e-switch-signature)
+(defun mu4e-goodies-switch-signature-by-rule ()
+  "Switch the draft's signature according to the rules defined in
+  mu4e-goodies-signature-switch-rules"
+  (let ((msg mu4e-compose-parent-message)
+        (rules mu4e-goodies-signature-switch-rules))
+    (while (and rules 
+                (not (mu4e-message-contact-field-matches msg
+                                                         :from
+                                                         (caar rules))))
+      (setq rules (cdr rules)))
+    (when rules
+      (mu4e-goodies-switch-signature (cdar rules)))))
+
+
+(define-key mu4e-compose-mode-map "\C-cs" 'mu4e-goodies-switch-signature)
 
 
 (provide 'mu4e-goodies-signature-switch)
