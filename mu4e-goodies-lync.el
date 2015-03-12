a18d408ac0fd51f7aee300d77f20c3707aa92978
panjie <panjie@gmail.com>
2015-03-12 09:36:08 +0800

    change to mu4e-goodies-lync-chat which now can take nil or email address(es) as parameter

--- mu4e-goodies-lync.el
+++ mu4e-goodies-lync.el
@@ -18,14 +18,18 @@
 (defun mu4e-goodies-lync-chat (&optional emails)
   "Lync with the email address at point if parameter email is not given"
   (interactive)
-  (browse-url (if (and emails (> (length emails) 0))
+  (browse-url (if (and (listp emails) (> (length emails) 0))
+                  ;; an email list
                   (let ((uri "im:"))
                     (dolist (email emails)
                       (unless (string= email user-mail-address) 
                         (setq uri (concat uri "<sip:" email ">"))))
                     (mu4e-message "Lync with %s" uri)
                     uri)
-                (concat "sip:" (get-text-property (point) 'email)))))
+                (if (stringp emails)
+                    ;; a single email
+                    (concat "sip:" emails)
+                  (concat "sip:" (get-text-property (point) 'email))))))
 
 (define-key mu4e-view-contacts-header-keymap "L" 'mu4e-goodies-lync-chat)
 
